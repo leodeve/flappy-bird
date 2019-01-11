caa972476f6632a26170ea9559b6e728be158817
leodeve <46608797+leodeve@users.noreply.github.com>
2019-01-11 21:30:50 +0100

    Update AndroidLauncher.java

--- AndroidLauncher.java
+++ AndroidLauncher.java
@@ -1,7 +1,8 @@
+//from Youtube, Brent Aureli's - Code School. I added a score and a highscore.
+//https://www.youtube.com/watch?v=rzBVTPaUUDg&list=PLZm85UZQLd2TPXpUJfDEdWTSgszionbJy
 package com.android.game;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
